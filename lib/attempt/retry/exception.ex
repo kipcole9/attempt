f5e6b97486f53e7a0f1c6dd9366808fe1541a2b9
Kip Cole <kipcole9@gmail.com>
2018-01-05 10:38:57 +1100

    Add Ecto, Postgrex and DBConnection exceptions to exception retry classifier

--- lib/attempt/retry/exception.ex
+++ lib/attempt/retry/exception.ex
@@ -24,3 +24,43 @@ end
 defimpl Attempt.Retry.Exception, for: ArithmeticError do
   def retriable?(_exception), do: :reraise
 end
+
+if Code.ensure_loaded?(Ecto) do
+  defimpl Attempt.Retry.Exception, for: Ecto.Query.CastError do
+    def retriable?(_exception), do: :return
+  end
+
+  defimpl Attempt.Retry.Exception, for: Ecto.Query.CompileError do
+    def retriable?(_exception), do: :return
+  end
+
+  defimpl Attempt.Retry.Exception, for: Ecto.QueryError do
+    def retriable?(_exception), do: :return
+  end
+
+  defimpl Attempt.Retry.Exception, for: Ecto.SubQueryError do
+    def retriable?(_exception), do: :return
+  end
+
+  defimpl Attempt.Retry.Exception, for: Ecto.InvalidChangesetError do
+    def retriable?(_exception), do: :return
+  end
+end
+
+# TODO Some exceptions should retry ... need to decide what they
+# are - especially lock contention s
+if Code.ensure_loaded?(Postgrex) do
+  defimpl Attempt.Retry.Exception, for: Postgrex.Error do
+    def retriable?(_exception), do: :return
+  end
+end
+
+if Code.ensure_loaded?(DBConnection) do
+  defimpl Attempt.Retry.Exception, for: DBConnection.ConnectionError do
+    def retriable?(_exception), do: :retry
+  end
+
+  defimpl Attempt.Retry.Exception, for: DBConnection.TransactionError do
+    def retriable?(_exception), do: :retry
+  end
+end
